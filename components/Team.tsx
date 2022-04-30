6871f419c66da71b222a67476f9b6d07dacdf79f
Aruzo <dawid12391111@gmail.com>
2022-04-30 15:18:48 +0200

    created team component

---
+++ components/Team.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+const members = [
+  { name: "Gigantic", avatar: "gigantic.jpg", role: "Sniper" },
+  { name: "Brandan", avatar: "brandan.png", role: "Attacker" },
+  { name: "Minny", avatar: "minny.webp", role: "Captain" },
+  { name: "Unspeakable", avatar: "unspekable.webp", role: "Tank" },
+];
+
+const Team = () => {
+  return (
+    <section className="container py-8">
+      <h2 className="text-5xl font-bold text-brand-200">Meet our team</h2>
+      <p className="text-neutral-200">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </p>
+      <ul className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+        {members.map((member) => (
+          <li
+            className="p-8 rounded-lg bg-neutral-800"
+            key={member.name}
+          >
+            <div className="relative aspect-square">
+              <Image
+                className="rounded-lg"
+                src={`/images/avatars/${member.avatar}`}
+                layout="fill"
+              />
+            </div>
+            <h3 className="mt-2 text-xl font-medium">{member.name}</h3>
+            <div className="w-full mt-2 py-2 rounded-lg text-center bg-brand-200">
+              {member.role}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Team;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from "next";
 import Welcome from "../components/welcome/Welcome";
+import Team from "../components/Team";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>SinCity Banditz</title>
+      </Head>
       <Welcome />
+      <Team />
     </div>
   );
 };
