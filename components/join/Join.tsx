9ae122118320b4fef1996acb6d70fe46fc9f4365
Aruzo <dawid12391111@gmail.com>
2022-05-01 19:26:58 +0200

    added team request points

--- components/join/Join.tsx
+++ components/join/Join.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import axios from "axios";
 import { Form, Formik } from "formik";
 import {
+  points,
   joinInitialValues,
   joinValidationSchema,
 } from "../../helpers/constants";
@@ -8,6 +10,8 @@ import Input from "./Input";
 import Spinner from "./Spinner";
 
 const Join = () => {
+  const [isSent, setIsSent] = useState(false);
+
   return (
     <section id="join" className="py-16 bg-neutral-800">
       <div className="container">
@@ -16,11 +20,13 @@ const Join = () => {
           Lorem Ipsum has been the industrys standard dummy text ever since the
           1500s.
         </p>
-        <div className="flex gap-4 mt-4">
+        <div className="flex flex-col-reverse md:flex-row gap-8 mt-8">
           <Formik
             initialValues={joinInitialValues}
             validationSchema={joinValidationSchema}
-            onSubmit={async (values) => axios.post("/email", values)}
+            onSubmit={async (values) => {
+              await axios.post("/email", values).then(() => setIsSent(true));
+            }}
           >
             {({ isSubmitting }) => (
               <Form className="grid grid-cols-2 gap-4 w-full md:w-1/2">
@@ -41,15 +47,24 @@ const Join = () => {
                 <button
                   className="btn-brand py-4"
                   type="submit"
-                  disabled={isSubmitting}
+                  disabled={isSubmitting || isSent}
                 >
-                  {!isSubmitting && "Send Request"}
+                  {!isSubmitting && (isSent ? "Request Sent" : "Send Request")}
                   {isSubmitting && <Spinner />}
                 </button>
               </Form>
             )}
           </Formik>
-          <div>hello</div>
+          <ul className="flex flex-col gap-y-8 w-full md:w-1/2">
+            {points.map((point) => (
+              <li key={point.text} className="flex items-center gap-x-4">
+                <div className="p-2 rounded-full bg-brand">
+                  <point.Icon className="w-8 h-8" />
+                </div>
+                <p className="font-medium">{point.text}</p>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </section>
--- helpers/constants.ts
+++ helpers/constants.ts
@@ -1,4 +1,5 @@
 import * as yup from "yup";
+import { HeartIcon, GlobeIcon, PencilAltIcon } from "@heroicons/react/outline";
 
 export const navigation = [
   { name: "Home", location: "welcome" },
@@ -14,9 +15,24 @@ export const members = [
   { name: "Leroy", avatar: "brandan.png", role: "Something" },
 ];
 
+export const points = [
+  {
+    text: "You will need to be great at teamwork. You must treat everyone with up-most respect.",
+    Icon: HeartIcon,
+  },
+  {
+    text: "You will need to stand out from the rest and have awesome creativity!",
+    Icon: GlobeIcon,
+  },
+  {
+    text: "You will need to be able to take the drivers seat and get work done when requested.",
+    Icon: PencilAltIcon,
+  },
+];
+
 export const joinInitialValues = {
   username: "",
-  age: 0,
+  age: undefined,
   email: "",
   experience: "",
 };
